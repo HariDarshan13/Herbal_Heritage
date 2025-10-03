2261b922fdaf6b65e61b9620796846b0753d3679
HariDarshan13 <haridarshanhari123@gmail.com>
2025-10-03 16:25:51 +0530

    Update Register.tsx

--- frontend/src/pages/user/Register.tsx
+++ frontend/src/pages/user/Register.tsx
@@ -34,7 +34,7 @@ export default function Register() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://herbal-heritage-backendssss.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
